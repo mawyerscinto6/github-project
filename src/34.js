9de0cf5382054f0236df7374bff11c4abd391b59
mawyerscinto6 <mawyerscinto6@gmail.com>
2025-04-27 01:57:38 +0000

    Add 34.js

---
+++ src/34.js
@@ -0,0 +1,9 @@
+// Example of an unformatted string or variable assignment.
+const user = "John Doe";
+
+// Randomly select an integer between 1 and 100 (inclusive).
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Example of a simple math expression.
+let result = 5 * 3 - 2;
+console.log(result);
